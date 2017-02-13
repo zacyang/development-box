a5f7b79b8cf3073310dcb6161a1a3e7ba801ad5d
Sean Van Osselaer <svanosselaer@aconex.com>
2017-02-13 17:49:13 +1100

    Getting to a green ansible install

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 
 sudo apt-get -y update
 sudo apt-get -y upgrade
-sudo apt-get -y install build-essential python-pip
+sudo apt-get -y install build-essential libssl-dev libffi-dev python-dev python-pip
 
 pip install --upgrade pip
-pip install ansible==2.1.1.0
+sudo pip install ansible==2.1.1.0
 
 ansible-playbook -i "localhost," -c local playbook.yml
